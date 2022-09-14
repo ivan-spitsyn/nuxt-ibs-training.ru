da72702afc5f057ae905aecec181d9676aad1a06
Apte4ka112 <krainoff.alesha@yandex.ru>
2022-09-14 18:24:14 +0300

    Fixed fixed header styles

--- components/layout/LayoutHeaderSearch.vue
+++ components/layout/LayoutHeaderSearch.vue
@@ -29,12 +29,43 @@ const props = defineProps({
     </div>
   </div>
 </template>
-<style scoped>
+<style scoped lang="scss">
 .search {
   display: flex;
   justify-content: flex-start;
   align-items: center;
   height: 100px;
+  &__form {
+    form {
+      background: #fff;
+      overflow: hidden;
+      border-radius: 4px;
+      border: 1px solid #eee;
+      width: 530px;
+      display: flex;
+      align-items: stretch;
+      justify-content: flex-start;
+    }
+    .search-main {
+      height: 40px;
+      line-height: 40px;
+      margin: 0;
+      padding: 0 10px;
+      border: none;
+      outline: transparent;
+      box-sizing: border-box;
+      width: 100%;
+    }
+
+    input[name=submit] {
+      display: block;
+      border: none;
+      outline: transparent;
+      width: 80px;
+      /*background: url("/local/assets/css/../images/icons/search_form.svg") center / auto 60% no-repeat;*/
+      cursor: pointer;
+    }
+  }
 }
 
 .search-phrase {
@@ -44,60 +75,31 @@ const props = defineProps({
   align-items: center;
 }
 
-.search-phrase__label {
+.search-phrase{
+  &__list {
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+  &__item {
+    margin-left: 28px;
+    white-space: nowrap;
+    line-height: 1.6;
+  }
+  &__link {
+    color: #333;
+    text-decoration: none;
+  }
+
+
 }
 
-.search-phrase__list {
-  padding: 0;
-  list-style-type: none;
-  display: flex;
-  justify-content: flex-start;
-  align-items: center;
-  flex-wrap: wrap;
-}
-
-.search-phrase__item {
-  margin-left: 28px;
-  white-space: nowrap;
-  line-height: 1.6;
-}
-
-.search-phrase__link {
-  color: #333;
-}
-
-.search__form form {
-  background: #fff;
-  overflow: hidden;
-  border-radius: 4px;
-  border: 1px solid #eee;
-  width: 530px;
-  display: flex;
-  align-items: stretch;
-  justify-content: flex-start;
-}
-
-.search__form .search-main {
-  height: 40px;
-  line-height: 40px;
-  margin: 0;
-  padding: 0 10px;
-  border: none;
-  outline: transparent;
-  box-sizing: border-box;
-  width: 100%;
-}
-
-.search__form input[name=submit] {
-  display: block;
-  border: none;
-  outline: transparent;
-  width: 80px;
-  /*background: url("/local/assets/css/../images/icons/search_form.svg") center / auto 60% no-repeat;*/
-  cursor: pointer;
-}
 div.title-search-result {
   max-width: 100%;
 }
+
 
 </style>
